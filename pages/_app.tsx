0ee0a8c4a3e22029cbec4ca770588dec21a687ac
iueu <ilwelisaac@hotmail.com>
2022-12-30 01:02:46 -0300

    removing errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,10 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import { colors } from '../utils/colors';
 import Head from 'next/head';
+import { Container, Engine } from 'tsparticles-engine';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const particlesInit = useCallback(async engine => {
+  const particlesInit = useCallback(async (engine: Engine) => {
     console.log(engine);
     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
@@ -16,15 +17,11 @@ export default function App({ Component, pageProps }: AppProps) {
     await loadFull(engine);
   }, []);
 
-  const particlesLoaded = useCallback(async container => {
-  }, []);
-
   return (
     <>
       <Particles
         id="tsparticles"
         init={particlesInit}
-        loaded={particlesLoaded}
         options={{
           background: {
             color: {
@@ -69,7 +66,6 @@ export default function App({ Component, pageProps }: AppProps) {
               enable: true,
             },
             move: {
-              directions: "none",
               enable: true,
               outModes: {
                 default: "bounce",
